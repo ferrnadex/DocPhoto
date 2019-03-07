6b4ef8beaa18b810e4fbdadce72ea51aff6e01ae
FERNAN RUIZ SANTIAGO <Fruiz3292@intersg.edu>
2019-03-07 13:48:03 -0400

    Initial commit

---
+++ app/src/main/java/com/mobileteam/intersg/docphoto/MainActivity.java
@@ -0,0 +1,54 @@
+package com.mobileteam.intersg.docphoto;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //etRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+
+    }
+
+    public void photo(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(intent, 0);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+        Intent intent = new Intent(this, Preview.class);
+
+        intent.putExtra("resId", bitmap);
+
+        startActivity(intent);
+
+        //img.setImageBitmap(bitmap);
+    }
+
+}
---
+++ app/src/main/java/com/mobileteam/intersg/docphoto/Preview.java
@@ -0,0 +1,27 @@
+package com.mobileteam.intersg.docphoto;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Preview extends AppCompatActivity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+
+        img = findViewById(R.id.imageView);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle != null)
+        {
+            int res_id = bundle.getInt("resId");
+            img.setImageResource(res_id);
+        }
+    }
+
+
+}
