13ad6f1cdfd2112648acad4e67215a13fac912a0
FERNAN RUIZ SANTIAGO <Fruiz3292@intersg.edu>
2019-03-11 15:19:43 -0400

    Change Set builder.setCancelable(false) Create the Msg() function Declare cV_4 and set cV_4.setOnClickListener

--- app/src/main/java/com/mobileteam/intersg/docphoto/MainActivity.java
+++ app/src/main/java/com/mobileteam/intersg/docphoto/MainActivity.java
@@ -12,11 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     CardView cV_1;
+    CardView cV_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +35,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        cV_4 = findViewById(R.id.photo_card);
+
+        cV_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, 0);
+            }
+        });
         //etRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
-
-    }
-
-    public void photo(View view) {
-
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        startActivityForResult(intent, 0);
-
 
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT) // android suggestion
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
         // try catch: for prevent the fatal error when the user go back
         try {
 
@@ -63,17 +68,24 @@ public class MainActivity extends AppCompatActivity {
 
             //img.setImageBitmap(bitmap);*/
 
-            if(data != null)
+            // reference: https://stackoverflow.com/questions/28609527/null-intent-on-onactivityresult-when-using-mediastore-extra-output-for-action-im
+            if( resultCode == RESULT_OK )
             {
                 CharSequence gallerySelector[] = new CharSequence[]{"Doc Sale", "Doc Rent", "Doc Receipt"};
 
                 // reference:https://stackoverflow.com/questions/8605301/alertdialog-with-selector
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                //  If set to false it allows to cancel the dialog box by clicking on area outside the dialog else it allows.
+                // reference: https://abhiandroid.com/ui/alertdialog
+                builder.setCancelable(false);
+
                 builder.setTitle("Select Option");
                 builder.setItems(gallerySelector, new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Log.e("value is", "" + which);
+
                         switch (which) {
                             case 0:
                                 break;
@@ -86,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
                 });
                 builder.show();
             }
+            else
+            {
+                Msg();
+            }
 
         }
         catch (Exception e)
@@ -94,6 +110,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    private void Msg() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("The image will not be save.");
+        builder.show();
     }
 
 }
